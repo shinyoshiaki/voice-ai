97285c227bd5eca4050a13689072233aa16396c6
shinyoshiaki <shinyoshiaki2012@gmail.com>
2023-04-30 20:52:50 +0900

    emotion

--- apps/gpt-voice-server/src/domain/tts.ts
+++ apps/gpt-voice-server/src/domain/tts.ts
@@ -4,6 +4,8 @@ import { VoicevoxClient } from "../../../../libs/voicevox-client/src";
 const voicevox = new VoicevoxClient();
 
 export class TtsClient {
+  emotion = { neutral: 23, happy: 24, angry: 26, sad: 25, relaxed: 24 };
+
   constructor(private audio2Rtp: Audio2Rtp) {}
 
   removeEmotionTags(text: string) {
@@ -11,10 +13,18 @@ export class TtsClient {
     return text.replace(regex, "");
   }
 
+  getEmotionTags(text: string) {
+    const regex = /\[.*?\]/g;
+    return (text.match(regex) ?? ["[]"])[0].slice(1, -1);
+  }
+
   async speak(sentence: string) {
+    const emotion = this.getEmotionTags(sentence);
     sentence = this.removeEmotionTags(sentence);
 
-    const wav = await voicevox.speak(sentence);
+    const wav = await voicevox.speak(sentence, {
+      speaker: this.emotion[emotion] ?? this.emotion.neutral,
+    });
     await this.audio2Rtp.inputWav(wav);
   }
 }
--- libs/voicevox-client/src/client.ts
+++ libs/voicevox-client/src/client.ts
@@ -3,11 +3,20 @@ import axios from "axios";
 export class VoicevoxClient {
   readonly http = axios.create({ baseURL: "http://localhost:50021" });
 
-  async speak(text: string) {
-    const query = await this.http.post(`/audio_query?speaker=1&text="${text}"`);
+  constructor(private props: { speaker?: number; speedScale?: number } = {}) {
+    props.speaker = props.speaker ?? 1;
+    props.speedScale = props.speedScale ?? 1.5;
+  }
+
+  async speak(text: string, props: { speaker?: number } = {}) {
+    props.speaker = props.speaker ?? this.props.speaker;
+
+    const query = await this.http.post(
+      `/audio_query?speaker=${props.speaker}&text="${text}"`
+    );
     const res = await this.http.post(
-      `/synthesis?speaker=1`,
-      { ...query.data, speedScale: 1.8 },
+      `/synthesis?speaker=${props.speaker}`,
+      { ...query.data, speedScale: this.props.speedScale },
       {
         responseType: "arraybuffer",
       }
