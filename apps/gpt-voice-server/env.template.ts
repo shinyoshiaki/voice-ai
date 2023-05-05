86124294f63bb0b5a079db7d2a842d78cd2f1ae0
shinyoshiaki <shinyoshiaki2012@gmail.com>
2023-05-05 21:33:39 +0900

    use gpt-3.5-turbo

--- apps/gpt-voice-server/env.template.ts
+++ apps/gpt-voice-server/env.template.ts
@@ -2,6 +2,6 @@ export const env = {
   modelPath: __dirname + "/../../vosk-model",
   voiceServer: { port: 8888 },
   endpoint: "ws://localhost:8888",
-  gptModel: "gpt-4",
+  gptModel: "gpt-3.5-turbo",
 };
 export type Env = typeof env;
