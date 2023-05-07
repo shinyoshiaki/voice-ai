357197e14126c73434bac5e726eb5ffac36999e1
shinyoshiaki <shinyoshiaki2012@gmail.com>
2023-05-07 19:39:33 +0900

    refactor

--- libs/rtp2text/src/session.ts
+++ libs/rtp2text/src/session.ts
@@ -1,36 +1,36 @@
 import { OpusEncoder } from "@discordjs/opus";
 import { randomUUID } from "crypto";
 import { Event } from "rx.mini";
-import { RtpPacket } from "werift";
+import { RtpPacket } from "werift-rtp";
 
-export class Session {
+export class RecognizeSession {
   readonly id = randomUUID();
   readonly onText = new Event<[{ result?: string; partial?: string }]>();
 
   private readonly encoder = new OpusEncoder(48000, 1);
   private prevPartial = "";
 
-  private constructor(private rec: any) {}
+  private constructor(private recognizer: any) {}
 
   async inputRtp(rtp: RtpPacket) {
     const decoded = this.encoder.decode(rtp.payload);
     this.recognize(decoded);
   }
 
-  static async Create(rec: any) {
-    const session = new Session(rec);
+  static async Create(recognizer: any) {
+    const session = new RecognizeSession(recognizer);
     return session;
   }
 
-  recognize(pcm: Buffer) {
-    if (this.rec.acceptWaveform(pcm)) {
-      const result: string = this.rec.result().text;
+  private recognize(pcm: Buffer) {
+    if (this.recognizer.acceptWaveform(pcm)) {
+      const result: string = this.recognizer.result().text;
       if (result) {
         const formatted = result.replaceAll(/\s+/g, "");
         this.onText.execute({ result: formatted });
       }
     } else {
-      const partial = this.rec.partialResult().partial;
+      const partial = this.recognizer.partialResult().partial;
       if (partial) {
         const formatted = partial.replaceAll(/\s+/g, "");
         if (this.prevPartial === formatted) {
